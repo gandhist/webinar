c3db9096f4e06eac9017f8e0b8846429d7b1b26c
noer <noer@csimandiri.com>
2020-07-20 12:52:05 +0700

    First Commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>Sertifikat P3SM</b>- Login</a>
+    <a href="../../index2.html"><b>Sertifikat P3SM</b>-Login</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
