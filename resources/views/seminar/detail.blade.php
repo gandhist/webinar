ed612f945c5c877870dc0d760a378e28a2aed8d3
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-08-06 19:02:53 +0700

    modal detail seminar

--- resources/views/seminar/detail.blade.php
+++ resources/views/seminar/detail.blade.php
@@ -35,16 +35,6 @@
               
                 <h1 style="margin-bottom:50px;">Seminar</h1>
 
-                <div class="row" style="margin-bottom:50px;">
-                    <div class="col-md-2"></div>
-
-                    <div class="col-md-8">
-                        <img src="{{$seminar->link ? url($seminar->link) : ''}}" alt="Brosur Seminar"
-                            class="img-thumbnail center" style="width:50%">
-                    </div>
-
-                    <div class="col-md-2"></div>
-                </div>
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group {{ $errors->first('nama_seminar') ? 'has-error' : '' }}">
@@ -224,6 +214,19 @@
                     </div>
                 </div>  
 
+                <div class="row" style="margin-bottom:50px;">
+                    <div class="col-md-12">
+                        <div class="form-group {{ $errors->first('instansi_penyelenggara') ? 'has-error' : '' }} ">
+                            <label>Browsur Seminar</label>
+                            <div>
+                                <a data-toggle="modal" data-target="#myModal">
+                                    Lihat <i class="fa fa-external-link" aria-hidden="true"></i>
+                                </a> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 {{-- instansi original --}}
                 {{--
                 <div class="row">
@@ -310,6 +313,31 @@
         </div> {{-- Jumbotron --}}
       </div> {{-- Container-fluid --}}
     </div> {{-- Box-Content --}}
+
+
+    {{-- Modal Foto --}}
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	  <div class="modal-dialog" role="document">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+	        <h4 class="modal-title" id="myModalLabel">Brosur Seminar</h4>
+	      </div>
+	      <div class="modal-body">
+	      	<center>	
+              <img src="{{$seminar->link ? url($seminar->link) : ''}}" alt="Brosur Seminar"
+                            class="img-thumbnail center" style="width:50%">
+	        </center>
+	      </div>
+	      <div class="modal-footer">
+	        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>	
+	      </div>
+	    </div>
+	  </div>
+	</div>
+
+{{-- Akhir Modal Foto --}}
 </section>
 
 <!-- Modal Lampiran -->
