ed592cf210498ab332656a001bd30751061e9d8f
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-08-10 14:57:13 +0700

    Nambah library excel

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -8,11 +8,14 @@ class ImportController extends Controller
 {
     //
     public function index() {
-        $seminar = Seminar::all();
-        dd($seminar);
+        $seminar = Seminar::where('is_actived','1')->get();
+        return view('import.index')->with(compact('seminar'));
     }
 
     public function import(Request $request) {
-
+        $request->validate([
+            'seminar' => 'required',
+            'file' => 'required|mimes:xlsx|max:2048'
+        ]);
     }
 }
