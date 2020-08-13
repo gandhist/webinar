c0b809d5bc367c3f6b7c6bb6ce71b06251014e37
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-08-13 15:06:31 +0700

    create tuk

--- resources/views/tuk/create.blade.php
+++ resources/views/tuk/create.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
 
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>
+        Tambahkan Seminar
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#"> Daftar</a></li>
+        <li class="active"><a href="#"> Seminar</a></li>
+        <li class="active"><a href="#"> Tambah</a></li>
+    </ol>
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <!-- Default box -->
+    <div class="box box-content">
+        <div class="container-fluid">
+            <div class="jumbotron" style='padding-top:1px'>
+                <h1 style="margin-bottom:50px;">Seminar</h1>
+                <form method="POST" action="{{ url('seminar/store') }}" enctype="multipart/form-data">
+                @csrf
+
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- END Default BOX -->
+</section>
+<!-- End MAIN -->
 @endsection
