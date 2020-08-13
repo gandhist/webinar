85251a9f19a135bb834f41a38d79b48c936a125d
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-08-13 16:56:39 +0700

    create tuk

--- app/Http/Controllers/TUKController.php
+++ app/Http/Controllers/TUKController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\TUKModel;
+use App\ProvinsiModel;
+use App\KotaModel;
 
 class TUKController extends Controller
 {
@@ -11,5 +14,38 @@ class TUKController extends Controller
     public function index() {
         $tuk = TUKModel::all();
         return view('tuk.index')->with(compact('tuk'));
+    }
+
+    public function create() {
+        $provinsi = ProvinsiModel::all();
+        $kota = KotaModel::all();
+        return view('tuk.create')->with(compact('provinsi','kota'));
+    }
+
+    public function store(Request $request) {
+        $request->validate([
+            'nama_tuk'  => 'required|min:3|max:100',
+            'alamat'    => 'required|min:3|max:100',
+            'provinsi'  => 'required',
+            'kota'      => 'required',
+            'pengelola' => 'sometimes|nullable|min:3|max:100',
+            'email'     => 'sometimes|nullable|email',
+            'no_hp'     => 'sometimes|nullable|',
+            'website'   => 'sometimes|nullable|'
+        ]);
+
+        $data   = new TUKModel;
+        $data->nama_tuk  = $request->nama_tuk       ;
+        $data->alamat    = $request->alamat         ;
+        $data->prov      = $request->provinsi       ;
+        $data->kota      = $request->kota           ;
+        $data->pengelola = $request->pengelola      ;
+        $data->email     = $request->email          ;
+        $data->no_hp     = $request->no_hp          ;
+        $data->web       = $request->website        ;
+
+        $data->created_by = Auth::id();
+
+        $data->save();
     }
 }
