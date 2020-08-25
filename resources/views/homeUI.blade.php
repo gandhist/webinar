3f793972aad40f781d2fabd729d80e3a71ea2ec2
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-08-25 19:33:47 +0700

    bikim huruf G

--- resources/views/homeUI.blade.php
+++ resources/views/homeUI.blade.php
@@ -25,6 +25,12 @@
 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.bootstrap4.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+<script>
+    FontAwesomeConfig = { autoReplaceSvg: false }
+</script>
 <div class="container">
 
 
@@ -36,7 +42,7 @@
             @if(Auth::guest())
             <button href="#" class="btn btn-success btn-border-filled" id="login" >Login</button>
             <a href="{{url('login/google')}}" style="color: white" class="btn btn-danger">
-                </i> Login with Google Account
+                <i class="fa fa-google fa-fw"></i> Login with Google Account
             </a>
             @endif
 			{{-- <a href="{{ url('infoseminar') }}" class="btn btn-success">Daftar Seminar</a> --}}
@@ -118,13 +124,15 @@
 @endsection
 @push('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> --}}
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.5/js/responsive.bootstrap4.min.js"></script>
+
 <script type="text/javascript" >
+
 	$('.login').hide();
 	$('.welcome').fadeIn('slow');
 	// $('.seminar').hide();
