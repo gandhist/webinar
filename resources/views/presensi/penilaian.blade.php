b0908b3225654bea3582ed228b0da32d7f65beb1
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-08-29 11:47:43 +0700

    sangat baiiiiik

--- resources/views/presensi/penilaian.blade.php
+++ resources/views/presensi/penilaian.blade.php
@@ -101,7 +101,7 @@
                                         title = 'Baik'
                                         @break
                                     @case(5)
-                                        title = 'Sangat Baik'
+                                        title = 'Luar Biasa'
                                         @break
                                     @default
                                         title = 'Tidak tersedia'
@@ -149,7 +149,7 @@
                                             title = 'Baik'
                                             @break
                                         @case(5)
-                                            title = 'Sangat Baik'
+                                            title = 'Luar Biasa'
                                             @break
                                         @default
                                             title = 'Tidak tersedia'
@@ -198,7 +198,7 @@
                                             title = 'Baik'
                                             @break
                                         @case(5)
-                                            title = 'Sangat Baik'
+                                            title = 'Luar Biasa'
                                             @break
                                         @default
                                             title = 'Tidak tersedia'
@@ -316,8 +316,8 @@
             case '5':
                 // code block
                 el = document.getElementById("for-"+nama);
-                el.textContent = 'Sangat Baik';
-                // $("#for-"+nama).val('Sangat Baik');
+                el.textContent = 'Luar Biasa';
+                // $("#for-"+nama).val('Luar Biasa');
                 // console.log($("#for-"+nama));
                 break;
             default:
