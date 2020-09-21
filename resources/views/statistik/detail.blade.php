7452b83a47583094aa6562b630ee73a62a6025ba
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-09-21 11:36:48 +0700

    ikan hiu makan tomat

--- resources/views/statistik/detail.blade.php
+++ resources/views/statistik/detail.blade.php
@@ -107,12 +107,16 @@
                         </div>
                         <div class="col-md-6">
                             <div class="row">
-                                <div class="col-md-6">
-                                    <div class="card card-shadow"><span>Blasting Dikirim:</span><div>{{$blasting->jumlah_kirim}}</div></div>
+                                <div class="col-md-4">
+                                    <div class="card card-shadow"><span>Target Blasting:</span><div>{{$blasting->jumlah_target}}</div></div>
                                     <div class="card card-shadow"><span>Link Diklik:</span><div>{{$blasting_diklik}}</div></div>
                                 </div>
-                                <div class="col-md-6">
+                                <div class="col-md-4">
+                                    <div class="card card-shadow"><span>Blasting Dikirim:</span><div>{{$blasting->jumlah_kirim}}</div></div>
                                     <div class="card card-shadow"><span>Klik Lebih dari Sekali:</span><div>{{$blasting_diklik2}}</div></div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="card card-shadow"><span>Tidak Dikirim:</span><div>{{(int)$blasting->jumlah_target - (int)$blasting->jumlah_kirim}}</div></div>
                                     <div class="card card-shadow"><span>Pendaftar dari Link:</span><div>{{$blasting_didaftar}}</div></div>
                                 </div>
                             </div>
@@ -147,12 +151,14 @@ var peserta = @json($peserta);
 var user = @json($user);
 var detail_blasting = @json($detail_blasting);
 var dikirim = @json($dikirim);
+var gagal = @json($gagal);
 
 var blasting_target = @json($blasting->jumlah_target);
 var blasting_dikirim = @json($blasting->jumlah_kirim);
 var blasting_diklik = @json($blasting_diklik);
 var blasting_diklik2 = @json($blasting_diklik2);
 var blasting_didaftar = @json($blasting_didaftar);
+var blasting_gagal = parseInt(blasting_target) - parseInt(blasting_dikirim);
 
 // console.log((blasting_target));
 // console.log(blasting_dikirim);
@@ -184,7 +190,7 @@ function ChartOption(title) {
     }
 };
 
-function DataBlasting(label, blasting, pendaftar, user, blasting_dikirim) {
+function DataBlasting(label, blasting, pendaftar, user, blasting_dikirim, blasting_gagal) {
     this.labels = label,
     this.datasets = [
         {
@@ -211,14 +217,20 @@ function DataBlasting(label, blasting, pendaftar, user, blasting_dikirim) {
         }, {
           label: "Blasting Dikirim",
           type: "bar",
-          backgroundColor: "rgba(230, 77, 0, 0.9)",
+          backgroundColor: "rgba(0, 0, 123, 0.9)",
           backgroundColorHover: "#3e95cd",
           data: blasting_dikirim
+        }, {
+          label: "Blasting Tidak Dikirim",
+          type: "bar",
+          backgroundColor: "rgba(255, 23, 0, 0.9)",
+          backgroundColorHover: "#3e95cd",
+          data: blasting_gagal
         },
     ]
 }
 
-function DataDetail(label, target, dikirim, diklik, diklik2, didaftar) {
+function DataDetail(label, target, dikirim, diklik, diklik2, didaftar, gagal) {
     this.labels = label,
     this.datasets = [
         {
@@ -230,6 +242,11 @@ function DataDetail(label, target, dikirim, diklik, diklik2, didaftar) {
           label: "Blasting Dikirim",
           backgroundColor: "springgreen",
           data: dikirim,
+        },
+        {
+          label: "Blasting Tidak Dikirim",
+          backgroundColor: "red",
+          data: gagal,
         },
         {
           label: "Link Blasting Di-klik",
@@ -263,13 +280,13 @@ $(document).ready(function () {
     var ctx = document.getElementById('blasting').getContext('2d');
     chartBlasting = new Chart(ctx, {
         type: 'bar',
-        data: new DataBlasting(label, detail_blasting, peserta, user, dikirim),
+        data: new DataBlasting(label, detail_blasting, peserta, user, dikirim, gagal),
         options: new ChartOption( "Grafik Aktivitas Kegiatan" ),
     });
     var ctx2 = document.getElementById('detail').getContext('2d');
     chartDetail = new Chart(ctx2, {
         type: 'bar',
-        data: new DataDetail(['Detail Blasting'],  [blasting_target], [blasting_dikirim] , [blasting_diklik.toString()],[blasting_diklik2.toString()],  [blasting_didaftar.toString()]),
+        data: new DataDetail(['Detail Blasting'],  [blasting_target], [blasting_dikirim] , [blasting_diklik.toString()],[blasting_diklik2.toString()],  [blasting_didaftar.toString()], [blasting_gagal.toString()]),
         options: new ChartOption( "Grafik Detail Blasting" ),
     });
     // var ctx = document.getElementById('info').getContext('2d');
