2d3d84c5a76f36adc230aa6ab63cad88f91eaa69
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-09-23 16:29:06 +0700

    josh

--- app/KantorModel.php
+++ app/KantorModel.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class KantorModel extends Model
 {
     use SoftDeletes;
-    protected $table = "ms_kantor";
+    protected $table = "kantor";
 }
--- resources/views/kantor/index.blade.php
+++ resources/views/kantor/index.blade.php
@@ -18,7 +18,43 @@
     <!-- Default box -->
     <div class="box box-content">
         <div class="box-body">
-
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <table id="data-tables" class="table table-striped table-bordered dataTable customTable">
+                            <thead>
+                                <tr>
+                                    <th><i class="fa fa-check-square-o"></i></th>
+                                    <th>No.</th>
+                                    <th>Nama_Ktr</th>
+                                    <th>Level_K</th>
+                                    <th>Prov</th>
+                                    <th>Kota</th>
+                                    <th>Nama_Pimp</th>
+                                    <th>Kontak_P</th>
+                                    <th>Keterangan</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($kantor as $key)
+                                <tr>
+                                    <td style='text-align:center'><input type="checkbox" data-id="{{ $key->id }}" class="selection"
+                                        id="selection[]" name="selection[]"></td>
+                                    <td class="text-center">{{ $loop->iteration }}</td>
+                                    <td>{{$key->nama_kantor}}</td>
+                                    <td>{{$key->level}}</td>
+                                    <td>{{$key->prop}}</td>
+                                    <td>{{$key->kota}}</td>
+                                    <td>{{$key->nama_pimp}}</td>
+                                    <td>{{$key->kontak_p}}</td>
+                                    <td>{{$key->keterangan}}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </section>
