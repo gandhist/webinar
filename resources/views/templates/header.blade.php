af03644a3606d927e8c5617fe519851f45347ae5
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2020-12-15 16:32:30 +0700

    fix diplay

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -40,7 +40,7 @@
   <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
   <![endif]-->
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-blue sidebar-mini sidebar-collapse ">
 <!-- Site wrapper -->
 <div class="wrapper">
 @include('templates.navigation')
