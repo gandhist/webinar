43c4264e2b9001029cb7b187c097b2c8a7818e4b
gandhi <gandhisaputra13@gmail.com>
2021-01-25 16:21:49 +0700

    change snap url from env MIDTRANS_SNAP_URL

--- resources/views/pembayaran/index.blade.php
+++ resources/views/pembayaran/index.blade.php
@@ -183,9 +183,14 @@
 <script src="https://cdn.datatables.net/responsive/2.2.5/js/responsive.bootstrap4.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-<script
+{{-- <script
       type="text/javascript"
       src="https://app.sandbox.midtrans.com/snap/snap.js"
+      data-client-key="{{config('services.midtrans.clientKey')}}"
+></script> --}}
+<script
+      type="text/javascript"
+      src="{{ env('MIDTRANS_SNAP_URL') }}"
       data-client-key="{{config('services.midtrans.clientKey')}}"
 ></script>
 <script>
